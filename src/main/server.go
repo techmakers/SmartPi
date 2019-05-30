9f347d713b64b4dff885e281935c2eed198af2cd
Carlo Cassinari <carlo@techmakers.io>
2019-05-30 11:34:52 +0200

    nolog

--- src/main/server.go
+++ src/main/server.go
@@ -170,7 +170,7 @@ func getInfos(w http.ResponseWriter, r *http.Request) {
 func loggingMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         // Do stuff here
-        log.Println(r.RequestURI)
+        // log.Println(r.RequestURI)
         // Call the next handler, which can be another middleware in the chain, or the final handler.
 /*
         u := smartpi.NewUser()
